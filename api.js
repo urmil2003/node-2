ca3b6e2d8200c85748529136e0b446c9dd8b3640
urmil2003 <137593255+urmil2003@users.noreply.github.com>
2023-07-19 12:02:41 +0530

    Add files via upload

---
+++ api.js
@@ -0,0 +1,9 @@
+
+const api=[
+    {name:'gajera urmil', mo:'9574476751'}, 
+    {name:'gajera meet', mo:'1698530716'},
+    {name:'gajera mrugesh', mo:'9909833525'}
+    
+]
+
+module.exports=api;
---
+++ index.js
@@ -0,0 +1,39 @@
+// video 8 ma file create karave
+// node moon install karava npm i nodemoon -g pachal  -g lagadavu atale node moon puri system ma inatall tahy jay 
+//  and macbook & ubanto  ma sudo npm i nodemoon -g
+
+//console.log("package.json")
+
+//color
+
+//const colors=require('colors')
+//console.log("package".yellow)
+
+// node module ni file delete thay jay to =>npm install .
+
+//console.warn("urmil")
+
+
+//const http = require('http');
+//const api =require('./api');
+//http.createServer((req, res)=>{
+  //res.writeHead(200,{'content-type':'application\json'});
+  //res.write(JSON.stringify(api));
+  //res.end();
+
+//}).listen(5600);
+
+
+// add and remove file 
+
+//const fs =require('fs');
+ //const input =process.argv;
+ //if(input[2]=='add')
+ //{
+  //fs.writeFileSync(input[3],input[4])
+ //}else if(input[2]=='remove')
+ //{
+  //fs.unlinkSync(input[3])
+ //}else{
+  //console.log("invalid input")
+ //}
